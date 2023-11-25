b1d31f34aeeeb072be86e253333340f4a7b8d05a
hwan515 <stophwan515@gmail.com>
2023-11-25 16:13:53 +0900

    modified:   src/api/BaseURL.js

--- src/api/BaseURL.js
+++ src/api/BaseURL.js
@@ -1,1 +1,4 @@
-export const URL = "http://54.254.200.196:5000/"
+export const URL ="https://rest-recipe-book-dptb.run.goorm.io"
+
+// "http://54.254.200.196:5000/"
+// "https://rest-recipe-book-dptb.run.goorm.io"
