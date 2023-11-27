d1508c4c17509791bf2ae24a5669cb49aa45a874
hwan515 <stophwan515@gmail.com>
2023-11-27 11:56:42 +0900

    modified:   src/pages/postForm/FormIngredients.jsx

--- src/pages/postForm/FormIngredients.jsx
+++ src/pages/postForm/FormIngredients.jsx
@@ -1,4 +1,4 @@
-import { useState, useEffect } from 'react';
+import React, { useState, useEffect, useCallback } from 'react';
 import { useNavigate, useLocation } from 'react-router-dom';
 import Button from '../../components/button/Button';
 import { styled } from 'styled-components';
